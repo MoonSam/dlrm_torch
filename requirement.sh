4ba057b8721f0fc67590ecfe321b941de93e60b3
MoonSam <ty5623@naver.com>
2022-12-30 16:04:28 +0900

    modify requirement.sh one more time

--- requirement.sh
+++ requirement.sh
@@ -2,5 +2,5 @@ conda install -c anaconda scikit-learn -y
 conda install -c conda-forge tensorboard -y
 conda install -c conda-forge tqdm -y
 git clone https://github.com/mlperf/logging.git mlperf-logging
-pip install -e mlperf-logging -y
+pip install -e mlperf-logging
 conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
